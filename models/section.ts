3bee2723921d338fa8a4c0bdf9d0a9670dc1773d
MilesLarkot <mileslarkot@proton.me>
2025-06-12 11:21:16 +0200

    deployment test ready

--- models/section.ts
+++ models/section.ts
@@ -1,13 +1,13 @@
-import mongoose from "mongoose";
-import FieldSchema from "./field";
+// import mongoose from "mongoose";
+// import FieldSchema from "./field";
 
-const SectionSchema = new mongoose.Schema({
-  name: String,
-  content: String,
-  fields: [FieldSchema],
-  description: String,
-  category: String,
-});
+// const SectionSchema = new mongoose.Schema({
+//   name: String,
+//   content: String,
+//   fields: [FieldSchema],
+//   description: String,
+//   category: String,
+// });
 
-export default mongoose.models.Section ||
-  mongoose.model("Section", SectionSchema);
+// export default mongoose.models.Section ||
+//   mongoose.model("Section", SectionSchema);
